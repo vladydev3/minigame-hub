004fefbbbd7c3b5c4dd646bd194e7f32849023f3
Vladimir Pinera <vladimir.pv@avangenio.com>
2024-12-12 11:27:27 -0500

    .

--- src/app/tic-tac-toe/page.tsx
+++ src/app/tic-tac-toe/page.tsx
@@ -22,16 +22,16 @@ export default function TicTacToe() {
     setWinner(null);
   }
 
-  function checkWinner(cells: any[]) {
+  function checkWinner(cells: ("X" | "0" | null)[]) {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
-      [6, 7, 8], 
+      [6, 7, 8],
       [0, 3, 6],
       [1, 4, 7],
-      [2, 5, 8], 
+      [2, 5, 8],
       [0, 4, 8],
-      [2, 4, 6], 
+      [2, 4, 6],
     ];
     for (let line of lines) {
       const [a, b, c] = line;
